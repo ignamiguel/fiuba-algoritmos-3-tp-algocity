75e3f01db5f32171953f8e8b07fd77189e543834
frantguerini@gmail.com <frantguerini@gmail.com@2169864b-be0f-ae21-311f-979817b4c7fe>
2014-11-21 22:03:32 +0000

    Tests de edificios que involucren danio. La creacion de Afectable, godzilla y terremoto

--- algo3_workspace/AlgoCity/src/algo3/algocity/modelo/Industria.java
+++ algo3_workspace/AlgoCity/src/algo3/algocity/modelo/Industria.java
@@ -8,6 +8,7 @@ public class Industria extends Edificios{
 		cargarConsumoElectrico(5);
 		cargarPuestosDeTrabajo(25);
 		cargarCosto(10);
+		cargarSalud(100);
 	}
 
 	private void cargarPuestosDeTrabajo(int puestosDeTrabajo) {
@@ -17,4 +18,14 @@ public class Industria extends Edificios{
 	public int obtenerPuestosDeTrabajo() {
 		return this.puestosDeTrabajo;
 	}
+
+	public void afectarseCon(Godzilla godzilla) {
+		
+		this.salud -= 40;
+		if (this.salud < 0){
+			this.salud = 0;
+		}
+		
+	}
+
 }
--- algo3_workspace/AlgoCity/tests/algo3/algocity/modelo/TestEdificios.java
+++ algo3_workspace/AlgoCity/tests/algo3/algocity/modelo/TestEdificios.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 
 
+
 public class TestEdificios {
 	
 	
@@ -85,6 +86,146 @@ public class TestEdificios {
 		Industria industria = new Industria();
 		Terreno terreno = new Terreno();
 		assertEquals(industria.puedoEn(terreno),true);
+	}
+	
+	@Test
+	public void testResidenciaCreadaTieneSalud100(){
+		Residencia residencia = new Residencia();
+		assertEquals(residencia.obtenerSalud(),100);
+	}
+	
+	@Test
+	public void testComercioCreadoTieneSalud100(){
+		Comercio comercio = new Comercio();
+		assertEquals(comercio.obtenerSalud(),100);
+	}
+	
+	@Test
+	public void testIndustriaCreadaTieneSalud100(){
+		Industria industria = new Industria();
+		assertEquals(industria.obtenerSalud(),100);
+	}
+	
+	@Test
+	public void testResidenciaDanadaPorGodzillaQuedaConSalud0(){
+		Residencia residencia = new Residencia();
+		Godzilla godzilla = new Godzilla();
+		residencia.afectarseCon(godzilla);
+		assertEquals(residencia.obtenerSalud(),0);
+	}
+	
+	@Test
+	public void testResidenciaDanada2vecesPorGodzillaQuedaConSalud0(){
+		Residencia residencia = new Residencia();
+		Godzilla godzilla = new Godzilla();
+		residencia.afectarseCon(godzilla);
+		residencia.afectarseCon(godzilla);
+		assertEquals(residencia.obtenerSalud(),0);
+	}
+	
+
+	@Test
+	public void testIndustriaDanadaPorGodzillaQuedaConSalud60(){
+		Industria industria = new Industria();
+		Godzilla godzilla = new Godzilla();
+		industria.afectarseCon(godzilla);
+		assertEquals(industria.obtenerSalud(),60);
+	}
+	
+	@Test
+	public void testIndustriaDanadaPorGodzilla2vecesQuedaConSalud20(){
+		Industria industria = new Industria();
+		Godzilla godzilla = new Godzilla();
+		industria.afectarseCon(godzilla);
+		industria.afectarseCon(godzilla);
+		assertEquals(industria.obtenerSalud(),20);
+	}
+	
+	@Test
+	public void testIndustriaDanadaPorGodzilla3vecesQuedaConSalud0(){
+		Industria industria = new Industria();
+		Godzilla godzilla = new Godzilla();
+		industria.afectarseCon(godzilla);
+		industria.afectarseCon(godzilla);
+		industria.afectarseCon(godzilla);
+		assertEquals(industria.obtenerSalud(),0);
+	}
+	
+	@Test
+	public void testComercioDanadoPorGodzillaQuedaConSalud25(){
+		Comercio comercio = new Comercio();
+		Godzilla godzilla = new Godzilla();
+		comercio.afectarseCon(godzilla);
+		assertEquals(comercio.obtenerSalud(),25);
+	}
+	
+	
+	@Test
+	public void testComercioDanadoPorGodzilla2VecesQuedaConSalud0(){
+		Comercio comercio = new Comercio();
+		Godzilla godzilla = new Godzilla();
+		comercio.afectarseCon(godzilla);
+		comercio.afectarseCon(godzilla);
+		assertEquals(comercio.obtenerSalud(),0);
+	}
+	
+	@Test
+	public void testResidenciaDanadaPorTerremotoDeDanio100QuedaConSalud0(){
+		Residencia residencia = new Residencia();
+		residencia.afectarseCon(new Terremoto());
+		assertEquals(residencia.obtenerSalud(),0);
+	}
+	
+	@Test
+	public void testIndustriaDanadaPorTerremotoDeDanio100QuedaConSalud0(){
+		Industria industria = new Industria();
+		industria.afectarseCon(new Terremoto());
+		assertEquals(industria.obtenerSalud(),0);
+	}
+	
+	@Test
+	public void testComercioDanadoPorTerremotoDeDanio100QuedaConSalud0(){
+		Comercio comercio = new Comercio();
+		comercio.afectarseCon(new Terremoto());
+		assertEquals(comercio.obtenerSalud(),0);
+	}
+	
+	@Test
+	public void testResidenciaDanadaPorTerremotoDeDanio25QuedaConSalud75(){
+		Residencia residencia = new Residencia();
+		Terremoto terremoto = new Terremoto();
+		terremoto.cargarDanio(25);
+		residencia.afectarseCon(terremoto);
+		assertEquals(residencia.obtenerSalud(),75);
+	}
+	
+	@Test
+	public void testIndustriaDanadaPorTerremotoDeDanio25QuedaConSalud75(){
+		Industria industria = new Industria();
+		Terremoto terremoto = new Terremoto();
+		terremoto.cargarDanio(25);
+		industria.afectarseCon(terremoto);
+		assertEquals(industria.obtenerSalud(),75);
+	}
+	
+	@Test
+	public void testComercioDanadoPorTerremotoDeDanio99QuedaConSalud1(){
+		Comercio comercio = new Comercio();
+		Terremoto terremoto = new Terremoto();
+		terremoto.cargarDanio(99);
+		comercio.afectarseCon(terremoto);
+		assertEquals(comercio.obtenerSalud(),1);
+	}
+	
+	@Test
+	public void testIndustriaDanadaPorTerremotoDeDanio20YPorGodzillaQuedaConSalud40(){
+		Industria industria = new Industria();
+		Terremoto terremoto = new Terremoto();
+		terremoto.cargarDanio(20);
+		industria.afectarseCon(terremoto);
+		assertEquals(industria.obtenerSalud(),80);
+		industria.afectarseCon(new Godzilla());
+		assertEquals(industria.obtenerSalud(),40);
 	}
 	
 }
