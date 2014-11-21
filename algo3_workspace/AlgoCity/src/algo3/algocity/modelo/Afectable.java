704ba3666e232d730ca9fe54145c65096d748e6c
frantguerini@gmail.com <frantguerini@gmail.com@2169864b-be0f-ae21-311f-979817b4c7fe>
2014-11-21 22:06:16 +0000

    No se subieron los nuevos cambios en la version anterior

---
+++ algo3_workspace/AlgoCity/src/algo3/algocity/modelo/Afectable.java
@@ -0,0 +1,8 @@
+package algo3.algocity.modelo;
+
+public interface Afectable {
+	
+	public void afectarseCon(Godzilla unGodzilla);
+	public void afectarseCon(Terremoto unTerremoto);
+
+}
---
+++ algo3_workspace/AlgoCity/src/algo3/algocity/modelo/Terremoto.java
@@ -0,0 +1,20 @@
+package algo3.algocity.modelo;
+
+public class Terremoto {
+
+	private int danio;
+	
+	public Terremoto(){
+	
+		this.danio = 100;
+	}
+	
+	//Solo para Tests previos a la propagacion del terremoto
+	public void cargarDanio(int danio){
+		this.danio = danio;
+	}
+	
+	public int obtenerDanio(){
+		return this.danio;
+	}
+}
