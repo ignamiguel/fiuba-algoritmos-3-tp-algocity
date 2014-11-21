1956eec7788c2c558d94948b1b3dc92894f9cb4e
frantguerini@gmail.com <frantguerini@gmail.com@2169864b-be0f-ae21-311f-979817b4c7fe>
2014-11-21 22:06:16 +0000

    No se subieron los nuevos cambios en la version anterior

---
+++ trunk/algo3_workspace/AlgoCity/tests/algo3/algocity/modelo/testGodzilla.java
@@ -0,0 +1,16 @@
+package algo3.algocity.modelo;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testGodzilla {
+
+	@Test
+	public void testGodzillaSeCreaConExito(){
+		Godzilla godzilla = new Godzilla();
+		assertNotNull(godzilla);
+	}
+	
+	
+}
