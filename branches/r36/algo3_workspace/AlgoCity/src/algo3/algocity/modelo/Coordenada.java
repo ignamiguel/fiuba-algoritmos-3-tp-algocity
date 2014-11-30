389eed7c242a6ccd692cbc6e1ecf30bdf3a1d11c
iglesiasnacho <iglesiasnacho@2169864b-be0f-ae21-311f-979817b4c7fe>
2014-11-30 19:44:24 +0000

    Agrego la excepcion de Coordenada y algunos tests también para Coordenada. Agrego los contenedores de Edificios, Centrales y Pozos al Juego. Agrego la clase Ventana a la vista.

--- branches/r36/algo3_workspace/AlgoCity/src/algo3/algocity/modelo/Coordenada.java
+++ branches/r36/algo3_workspace/AlgoCity/src/algo3/algocity/modelo/Coordenada.java
@@ -3,12 +3,20 @@ package algo3.algocity.modelo;
 public class Coordenada {
 	int x;
 	int y;
-	
-	public Coordenada(int x,int y){
+
+	public Coordenada(int x, int y) {
+		if (x < 0) {
+			throw new CoordenadaInvalidaExcepcion();
+		}
+		
+		if(y < 0 ){
+			throw new CoordenadaInvalidaExcepcion();
+		}
+
 		this.x = x;
 		this.y = y;
 	}
-	
+
 	public int obtenerX() {
 		return this.x;
 	}
@@ -17,22 +25,20 @@ public class Coordenada {
 		return this.y;
 	}
 
-	public void aumentarX(int a){
-        this.x = (this.x + a);
+	public void aumentarX(int a) {
+		this.x = (this.x + a);
 	}
 
-	public void aumentarY(int b){
-        this.y= (this.y + b);
+	public void aumentarY(int b) {
+		this.y = (this.y + b);
 	}
 
-	public void disminuirX(int a){
-        this.x= (this.x - a);
+	public void disminuirX(int a) {
+		this.x = (this.x - a);
 	}
 
-	public void disminuirY(int b){
-        this.y= (this.y -b);
+	public void disminuirY(int b) {
+		this.y = (this.y - b);
 	}
 
 }
-
-
