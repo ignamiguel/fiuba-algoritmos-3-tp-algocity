ef86de6d74a8856a2f113fcc847938620d40661c
iglesiasnacho <iglesiasnacho@2169864b-be0f-ae21-311f-979817b4c7fe>
2014-12-01 05:23:08 +0000

    Agrego la clase Configuracion para declarar los valores iniciales. Agrego más test a la clase Juego.

--- branches/r36/algo3_workspace/AlgoCity/src/algo3/algocity/modelo/Industria.java
+++ branches/r36/algo3_workspace/AlgoCity/src/algo3/algocity/modelo/Industria.java
@@ -1,18 +1,14 @@
 package algo3.algocity.modelo;
 
-public class Industria extends Edificio{
-	
-	private int puestosDeTrabajo;
-	
-	public Industria(){
-		cargarConsumoElectrico(5);
-		cargarPuestosDeTrabajo(25);
-		cargarCosto(10);
-		cargarSalud(100);
-	}
+public class Industria extends Edificio {
 
-	private void cargarPuestosDeTrabajo(int puestosDeTrabajo) {
-		this.puestosDeTrabajo = puestosDeTrabajo;
+	private int puestosDeTrabajo;
+
+	public Industria() {
+		this.consumoElectrico = Configuracion.CONSUMO_ELECTRICO_INDUSTRIA;
+		this.puestosDeTrabajo = Configuracion.EMPLEOS_INDUSTRIA;
+		this.costo = Configuracion.COSTO_INDUSTRIA;
+		this.salud = Configuracion.SALUD_INICIAL;
 	}
 
 	public int obtenerPuestosDeTrabajo() {
@@ -20,31 +16,29 @@ public class Industria extends Edificio{
 	}
 
 	public void afectarCon(Godzilla godzilla) {
-		
+
 		this.salud -= 40;
-		if (this.salud < 0){
+		if (this.salud < 0) {
 			this.salud = 0;
 		}
-		
+
 	}
 
 	public boolean puedoEn(Hectarea hectarea) {
 		return true;
 	}
-	
+
 	public boolean puedoEn(Agua agua) {
 		return false;
 	}
-	
+
 	public boolean puedoEn(Terreno terreno) {
 		return true;
 	}
-	
-	
 
 	public void brindarServicio(Hectarea hectarea) {
 		// No brinda servicios
-		
-	}	
+
+	}
 
 }
