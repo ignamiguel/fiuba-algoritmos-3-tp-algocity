78d31d87244f407d03790294e385ddac06931d94
colque.fabian@gmail.com <colque.fabian@gmail.com@2169864b-be0f-ae21-311f-979817b4c7fe>
2014-12-08 01:43:57 +0000

    Subi la parte de ant para mostrar que funciona bien, arregle el error que habia dicho antes

--- trunk/algo3_workspace/AlgoCity/tests/algo3/algocity/modelo/CoordenadaTest.java
+++ trunk/algo3_workspace/AlgoCity/tests/algo3/algocity/modelo/CoordenadaTest.java
@@ -1,5 +1,12 @@
 package algo3.algocity.modelo;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
 //import org.junit.Test;
 
 public class CoordenadaTest {
@@ -9,5 +16,37 @@ public class CoordenadaTest {
 		new Coordenada(-1, 0);
 
 	}*/
+	
+	@Test
+	public void construyoCoordenadaConXigual1eYigual5(){
+
+		Coordenada coor = new Coordenada(1,5);
+		
+		assertEquals(1, coor.obtenerX());
+		assertEquals(5, coor.obtenerY());
+		
+	}
+	
+	@Test
+	public void aumentoXen1AunaCoordenada(){
+
+		Coordenada coor = new Coordenada(1,5);
+		coor.aumentarX(1);
+		assertEquals(2, coor.obtenerX());
+		assertEquals(5, coor.obtenerY());
+		
+	}
+	
+	@Test
+	public void aumentoYen10AunaCoordenada(){
+
+		Coordenada coor = new Coordenada(1,5);
+		coor.aumentarY(7);
+		assertEquals(1, coor.obtenerX());
+		assertEquals(12, coor.obtenerY());
+		
+	}
+	
+	
 
 }
